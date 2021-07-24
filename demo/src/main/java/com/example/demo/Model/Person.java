1411d54ed2a3b11c43676a5ab6f6e558a50517d7
Moonriderx <luben1993@abv.bg>
2021-07-24 12:57:15 +0300

    Added more functionality

--- demo/src/main/java/com/example/demo/Model/Person.java
+++ demo/src/main/java/com/example/demo/Model/Person.java
@@ -1,5 +1,8 @@
 package com.example.demo.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
 import java.util.UUID;
 
 public class Person { // Our model we will be working with
@@ -7,7 +10,8 @@ public class Person { // Our model we will be working with
     private final UUID id;
     private final String name;
 
-    public Person(UUID id, String name) {
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
--- demo/src/main/java/com/example/demo/Service/PersonService.java
+++ demo/src/main/java/com/example/demo/Service/PersonService.java
@@ -2,17 +2,30 @@ package com.example.demo.Service;
 
 import com.example.demo.Model.Person;
 import com.example.demo.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service // more specific than @Component at this point
 public class PersonService {
 
     private final PersonDao personDao;
 
-    public PersonService(PersonDao personDao) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
         this.personDao = personDao;
     }
 
     public int addPerson(Person person) {
         return personDao.insertPerson(person);
     }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+
 
 }
